02770321ace10a30811ee3b9ecae616b68850241
Huiyi Li <huiyi.li@outlook.com>
2016-07-10 19:54:53 +0800

    update topology, add SplitBolt

--- src/main/java/com/alibaba/middleware/race/jstorm/bolt/SplitBolt.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/bolt/SplitBolt.java
@@ -4,10 +4,19 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import com.alibaba.middleware.race.RaceConfig;
+import com.alibaba.middleware.race.jstorm.tuple.OrderTuple;
+import com.alibaba.middleware.race.jstorm.tuple.PaymentTuple;
 import org.apache.log4j.Logger;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Created by zhaoz on 2016/7/10.
@@ -19,14 +28,58 @@ public class SplitBolt implements IRichBolt{
     public static final Logger LOG = Logger.getLogger(SplitBolt.class);
     private OutputCollector collector;
 
+    private Set<Long> taobaoOrder = new HashSet<>();
+    private Set<Long> tmallOrder = new HashSet<>();
+    private LinkedBlockingDeque<PaymentTuple> paymentBuffer = new LinkedBlockingDeque<>();
+
+    class BufferThread extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    PaymentTuple payment = paymentBuffer.take();
+
+                    if (taobaoOrder.contains(payment.getOrderId())) {
+                        collector.emit(RaceConfig.taobaoStream, new Values(payment));
+                    } else if (tmallOrder.contains(payment.getOrderId())) {
+                        collector.emit(RaceConfig.tmallStream, new Values(payment));
+                    } else {
+                        paymentBuffer.addFirst(payment);
+                        Thread.sleep(100);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
     @Override
     public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
         this.collector = collector;
+        BufferThread buffer = new SplitBolt.BufferThread();
+        buffer.start();
     }
 
     @Override
     public void execute(Tuple input) {
+        Object field1 = input.getValue(0);
+        Object field2 = input.getValue(1);
 
+        if (field1 != null) {
+           paymentBuffer.addAll((List<PaymentTuple>) field1);
+        } else {
+            List<OrderTuple> orderTuples = (List<OrderTuple>) field2;
+            for (OrderTuple order : orderTuples) {
+                if (order.getOrderType() == 0) { // taobao order
+                    taobaoOrder.add(order.getOrderId());
+                } else {
+                    tmallOrder.add(order.getOrderId());
+                }
+            }
+        }
     }
 
     @Override
@@ -36,6 +89,8 @@ public class SplitBolt implements IRichBolt{
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declareStream(RaceConfig.taobaoStream, new Fields("payment"));
+        declarer.declareStream(RaceConfig.tmallStream, new Fields("payment"));
 
     }
 
